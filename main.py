071260bd139526ef3ef8004b60b9eaae529960e8
JENNER MUCHER <99274333+JennerMucher@users.noreply.github.com>
2022-02-15 15:52:55 +0000

    Added mucher.py 6 and changed main.py

--- main.py
+++ main.py
@@ -34,11 +34,12 @@ import importlib
     
 import example0, example1, example2, example3
 import example4, example5
+import mucher
 
 betray = example1
 collude = example0
 
-modules = [example0, example1, example2, example3, example4, example5]
+modules = [example0, example1, example2, example3, example4, example5, mucher]
 
 for module in modules:
     importlib.reload(module)
