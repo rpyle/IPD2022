2e6b15a06318e7638811235d48b1ee69ffc8aac7
RopTamen <99274437+344546@users.noreply.github.com>
2022-02-16 16:51:10 +0000

    fixed logic

--- moyer.py
+++ moyer.py
@@ -27,6 +27,7 @@ def move(my_history, their_history, my_score, their_score):
   if round < 10:
     pass
   elif ((their_history[0] == 'c') and (their_history[1] == 'b') and (their_history[2] == 'c') and (their_history[3] == 'c') and (their_history[4] == 'b') and (their_history[5] == 'b') and (their_history[6] == 'b') and (their_history[7] == 'c') and (their_history[8] == 'b') and (their_history[9] == 'c')):
+    print(my_history[-1])
     state = 2
   #else:
     #state = 1
@@ -35,7 +36,7 @@ def move(my_history, their_history, my_score, their_score):
   #  print(their_history[0] + their_history[1] + their_history[2] + their_history[3] + #their_history[4] + their_history[5] + their_history[6] + their_history[7] + #their_history[8] + their_history[9])
 
   #print(str(state) + ', ' + str(round))
-  #round = round + 1
+  round = round + 1
 
   if (state == 1):
     #if 'b' in their_history[-20:]:
@@ -46,26 +47,6 @@ def move(my_history, their_history, my_score, their_score):
   elif (state == 2):
     return 'b'
   elif (state == 0):
-    '''if (len(their_history) == 0):
-      return 'c'
-    if (len(their_history) == 1):
-      return 'b'
-    if (len(their_history) == 2):
-      return 'c'
-    if (len(their_history) == 3):
-      return 'c'
-    if (len(their_history) == 4):
-      return 'b'
-    if (len(their_history) == 5):
-      return 'b'
-    if (len(their_history) == 6):
-      return 'b'
-    if (len(their_history) == 7):
-      return 'c'
-    if (len(their_history) == 8):
-      return 'b'
-    if (len(their_history) == 9):
-      return 'c'''
     if (round == 1):
       return 'c'
     elif (round == 2):
